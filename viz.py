f8e26a5f5fa8169f3dcdf836f5ba38ef8e8ef2f0
Dahyun Kang <dahyun.kang@postech.ac.kr>
2020-07-01 21:56:55 +0900

    Add plot_one()

---
+++ viz.py
@@ -0,0 +1,73 @@
+import os
+import argparse
+from PIL import Image
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--img_path", default=None, help="image path to visualize", type=str)
+parser.add_argument("--dataset_path", default='CUB_200_2011', help="directory of the original CUB dataset you download and extract", type=str)
+parser.add_argument("--plot_order", help="plotting order", default='randomly', choices=['randomly', 'in_order'])
+
+args = parser.parse_args()
+origin_path = os.path.join(os.getcwd(), args.dataset_path)
+image_folder_prefix = 'images'
+
+part_label_dict = ['back', 'beak', 'belly', 'breast', 'crown', 'forehead', 'left eye', 'left leg', 'left wing', 'nape', 'right eye', 'right leg', 'right wing', 'tail', 'throat']
+id2path = {}
+path2id = {}
+with open(os.path.join(origin_path, 'images.txt')) as f:
+    lines = f.readlines()
+    for line in lines:
+        index, path = line.strip().split()
+        index = int(index)
+        id2path[index] = path
+        id2path[path] = index
+
+id2part = {}
+with open(os.path.join(origin_path, 'parts', 'part_locs.txt')) as f:
+    lines = f.readlines()
+    for line in lines:
+        index, part_id, x, y, visible = line.strip().split()
+        index = int(index)
+        x = float(x)
+        y = float(y)
+        visible = int(visible)
+        if index not in id2part:
+            id2part[index] = []
+        id2part[index].append([x, y, visible])
+
+
+def img_path_abs(rel_path):
+    return os.path.join(origin_path, image_folder_prefix, rel_path)
+
+
+def plot_one(img_path):
+    id = id2path[img_path]
+    part_locs = id2part[id]
+    print(part_locs)
+    abs_path = img_path_abs(img_path)
+    p = Image.open(abs_path)
+
+    plt.imshow(p)
+    for i, (x, y, visible) in enumerate(part_locs):
+        if visible:
+            label = '{} (o)'.format(part_label_dict[i])
+            plt.scatter(x, y, marker='+', label=label)
+        else:
+            label = '{} (x)'.format(part_label_dict[i])
+            plt.scatter(-1, -1, marker='+', label=label, visible=False)
+    plt.legend()
+    plt.show()
+
+
+def plot_all(plot_order):
+    pass
+
+
+if args.img_path is not None:
+    path_seperated = args.img_path.split(os.path.sep)
+    img_path = os.path.join(path_seperated[-2], path_seperated[-1])
+    plot_one(img_path)
+else:
+    plot_all(plot_order=args.plot_order)
+
